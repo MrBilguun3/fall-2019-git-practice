a7be70fff68a9ce110079d0f58e9ca6f920f3274
MrBilguun3 <40651006+MrBilguun3@users.noreply.github.com>
2020-03-18 20:57:25 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("World! ");
         System.out.println("Staying home...");
+        System.out.println("World! ");
+        System.out.println("Staying home...");
 
     }
 }
